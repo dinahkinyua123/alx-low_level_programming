827c3246d208da652b43114ec1cae0a1d2e1588e
dinakinyua123 <dayokishs@gmail.com>
2023-03-16 07:11:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints a line with puts functions
+ * Return: 0 (perfect)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzel");
+return (0);
+}
