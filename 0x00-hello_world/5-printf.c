561d7718341f0f9e7b518a5d86fc7eeb8458c0d8
dinakinyua123 <dayokishs@gmail.com>
2023-03-16 10:30:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints printf functions
+ * Return: 0(perfect)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n")
+	return (0);
+}	
