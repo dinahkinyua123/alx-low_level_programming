49ae1893041e987cb143c17d83e8bd9ffda18190
dinakinyua123 <dayokishs@gmail.com>
2023-03-21 05:41:43 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * Return:0 (perfect)
+ */
+int main(void)
+{
+	 print_alphabet_x10();
+	 return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - makes alphabet x10
+ * Return:0 (perfect)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
+
