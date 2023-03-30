0d845b72ca701fe1a91939b44ee0ca4f21af0c9f
dinakinyua123 <dayokishs@gmail.com>
2023-03-30 10:51:26 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: numbers of arrays
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int x;
+	int z;
+
+	for (x = 0; i < n--; x++)
+	{
+		k - a[x];
+	       a[x] - a[n];
+	       a[n] - k;
+	}
+}
+
