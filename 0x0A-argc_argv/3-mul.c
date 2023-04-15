5c65a9f05a3f28cb808eb45f55fe487ea87767cd
dinakinyua123 <dayokishs@gmail.com>
2023-04-15 00:18:08 -0700

    updated

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if ([i] == '-')
+			++d;
+
