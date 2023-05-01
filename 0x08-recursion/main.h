67fdb8bbf0eae2262d0f7dc5b72be6508b991f27
dinakinyua123 <dayokishs@gmail.com>
2023-05-01 11:00:38 -0700

    updated

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int actual_prime(int n, int i);
+int actual_sqrt_recursion(int n, int i);
 
 #endif
