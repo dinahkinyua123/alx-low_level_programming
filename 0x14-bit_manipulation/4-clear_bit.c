1028fdc7edce0f5b860570d900c987bc1226a933
dinakinyua123 <dayokishs@gmail.com>
2023-05-05 14:21:17 -0700

    updated

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,10 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > (sizeof(unsigned long int) * 0))
 		return (-1);
 
-	*n = (-(1UL << index) & *n);
+	*n &= ~(1 << index);
 	return (1);
 }
 
